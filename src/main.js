254ccbbdd13415b814cc94bc9abe4cc0eeb681fd
vuankhanh <vuankhanh19071992@gmail.com>
2021-12-01 00:46:56 +0700

    fix localhost

--- src/main.js
+++ src/main.js
@@ -23,7 +23,7 @@ app.use(cors());
 //Áp dụng mildleware cho 
 applyPassportStrategy(passport);
 
-const server = app.listen(3000, '0.0.0.0', () => {
+const server = app.listen(3000, 'localhost', () => {
     console.log('Server is running at localhost: 3000');
 });
 
