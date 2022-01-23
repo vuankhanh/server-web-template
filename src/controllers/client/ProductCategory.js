f89865ab91bb768afbcb071999f3ecc467882e10
Vu An Khanh <vuankhanh19071992@gmail.com>
2022-01-23 15:54:02 +0700

    Để tạm ảnh cho product category

--- src/controllers/client/ProductCategory.js
+++ src/controllers/client/ProductCategory.js
@@ -1,10 +1,26 @@
 const ProductCategory = require('../../models/ProductCategory');
+const Product = require('../../models/Product');
 
 module.exports = async(req, res)=>{
     try {
-        let productCategorys = await ProductCategory.model.ProductCategory.find({});
-        res.status(200).json(productCategorys);
+        let productCategories = await ProductCategory.model.ProductCategory.find({}).lean();
+        for(let i=0; i<=productCategories.length-1; i++){
+            let productCategory = productCategories[i];
+            let condition = { 'category.route': productCategory.route };
+            const product = await Product.model.Product.findOne(
+                condition,
+                {
+                    thumbnailUrl: 1
+                }
+            );
+            if(product){
+                productCategories[i].thumbnailUrl = product.thumbnailUrl;
+            }
+        }
+        // console.log(productCategories);
+        return res.status(200).json(productCategories);
     } catch (error) {
+        console.log(error);
         return res.status(500).json({ message: 'Something went wrong' });
     }
 }
